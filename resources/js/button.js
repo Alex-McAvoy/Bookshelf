c9798616829d5a5ec55db99add08e68eeecf3ac3
Alex-McAvoy <931772462@qq.com>
2026-03-14 22:59:55 +0800

    [Fix] 修复了过滤器active显示错误

--- resources/js/button.js
+++ resources/js/button.js
@@ -39,7 +39,7 @@ function resetPageFilter() {
 }
 // 重置字数过滤器
 function resetWordFilter() {
-    $("#readPageOptions .option").removeClass("active");
+    $("#readWordOptions .option").removeClass("active");
     $("#word-all").addClass("active");
 }
 
@@ -51,11 +51,17 @@ function refreshBookList(options) {
     var wordFilter = options.wordFilter;
 
     if (mediaFilter === "纸质书") {
-        $("#readPageOptions").show();
         $("#readWordOptions").hide();
+        resetWordFilter();
+        wordFilter = null;
+
+        $("#readPageOptions").show();
         pageFilter = pageFilter || "page-all";
     } else if (mediaFilter === "电子书") {
         $("#readPageOptions").hide();
+        resetPageFilter();
+        pageFilter = null;
+
         $("#readWordOptions").show();
         wordFilter = wordFilter || "word-all";
     }
@@ -65,6 +71,7 @@ function refreshBookList(options) {
         resetPageFilter();
         resetWordFilter();
         pageFilter = null;
+        wordFilter = null;
     }
 
     $("#readBookList").empty();
@@ -76,107 +83,3 @@ function refreshBookList(options) {
         showRating: true
     }));
 }
-
-
-
-
-
-// 为某个元素移除已有的class并设置新的class
-function setOption(elementId, optionName, optionValue) {
-    var $el = $("#" + elementId);
-    // 移除已有的 optionName-* class
-    $el.removeClass(function (index, className) {
-        return (className.match(new RegExp(optionName + "-\\S+", "g")) || []).join(' ');
-    });
-    // 添加新的 class
-    $el.addClass(optionName + "-" + optionValue);
-}
-
-
-// 获取媒介数量
-function getMediaCount(bookList, media) {
-    if (!bookList) return 0;
-    return bookList.filter(b => media === "all" || b.media === media).length;
-}
-
-// 获取页数范围数量
-function getPagesRangeCount(bookList, pagesRange) {
-    if (!bookList) return 0;
-    return bookList.filter(b => pagesRange === "all" || getPagesRange(b.pages) === pagesRange).length;
-}
-
-// 渲染媒介按钮
-function renderMediaOptionButton(media, bookList, bookListId) {
-    var mediaCount = getMediaCount(bookList, media);
-    if (mediaCount === 0 && media !== "all") return "";
-
-    var btnHtml = "<span class='text'>" + media + "</span>";
-    if (media !== "all")
-        btnHtml += "<span class='count'>" + mediaCount + "</span>";
-
-    return $("<div>")
-        .addClass("option media-" + media)
-        .html(btnHtml)
-        .data("media", media)
-        .on("click", function () {
-            setOption(bookListId, "media", media);
-            refreshBookList(bookListId);
-        });
-}
-
-// 渲染页数范围按钮
-function renderPagesRangeOptionButton(pagesRange, bookList, bookListId) {
-    var rangeCount = getPagesRangeCount(bookList, pagesRange);
-    if (rangeCount === 0 && pagesRange !== "all") return "";
-
-    var btnHtml = "<span class='text'>" + pagesRange + "</span>";
-    if (pagesRange !== "all") btnHtml += "<span class='count'>" + rangeCount + "</span>";
-
-    return $("<div>")
-        .addClass("option pagesRange-" + pagesRange)
-        .html(btnHtml)
-        .data("pagesRange", pagesRange)
-        .on("click", function () {
-            setOption(bookListId, "pagesRange", pagesRange);
-            refreshBookList(bookListId);
-        });
-}
-
-
-
-// 获取选项值
-function getOption(elementId, optionName) {
-    var className = $("#" + elementId).attr("class") || "";
-    var result = null;
-    className.split(/\s+/).forEach(function (cls) {
-        var parts = cls.split("-");
-        if (parts.length === 2 && parts[0] === optionName) {
-            result = parts[1];
-        }
-    });
-    return result;
-}
-
-// 重新渲染书架
-// function refreshBookList(bookListId) {
-//     var $collection = $("#" + bookListId);
-
-//     var media = getOption(bookListId, "media");
-//     var pagesRange = getOption(bookListId, "pagesRange");
-//     var order = getOption(bookListId, "order") || "ReadYearAndRating";
-
-//     // 获取书籍列表
-//     var bookList = window.BOOK_COLLECTIONS[bookListId] || [];
-
-//     var renderedHtml = renderBookList(bookList, {
-//         mediaFilter: media && media !== "all" ? media : null,
-//         showPagesOrWords: true,
-//         showPublisher: true,
-//         showISBN: true,
-//         showImage: true,
-//         showMedia: true,
-//         order: order
-//     });
-
-//     $collection.find("> #"+bookListId+"BookList, > div.bookList, > div:last-child").first().html(renderedHtml);
-// }
